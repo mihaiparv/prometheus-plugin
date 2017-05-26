ffe908f5aa83f7bb248c23cee0eebf6897326c61
Mihai Parv <mihai.parv@gmail.com>
2017-05-26 23:41:25 +0300

    Replaced dropwizard metrics with the default hotspot metrics which were updated to latest version.

--- src/main/java/org/jenkinsci/plugins/prometheus/rest/PrometheusAction.java
+++ src/main/java/org/jenkinsci/plugins/prometheus/rest/PrometheusAction.java
@@ -1,20 +1,15 @@
 package org.jenkinsci.plugins.prometheus.rest;
 
-import hudson.Extension;
-import hudson.model.RootAction;
-import hudson.model.UnprotectedRootAction;
-import hudson.security.Messages;
-import hudson.security.Permission;
-import hudson.util.HttpResponses;
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.dropwizard.DropwizardExports;
-import jenkins.metrics.api.Metrics;
-import jenkins.model.Jenkins;
-import org.acegisecurity.Authentication;
 import org.jenkinsci.plugins.prometheus.JobCollector;
 import org.jenkinsci.plugins.prometheus.MetricsRequest;
 import org.jenkinsci.plugins.prometheus.config.PrometheusConfiguration;
 import org.kohsuke.stapler.StaplerRequest;
+
+import hudson.Extension;
+import hudson.model.UnprotectedRootAction;
+import hudson.util.HttpResponses;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.hotspot.DefaultExports;
 
 @Extension
 public class PrometheusAction implements UnprotectedRootAction {
@@ -38,27 +33,13 @@ public class PrometheusAction implements UnprotectedRootAction {
 
     public Object doDynamic(StaplerRequest request) {
         if (request.getRestOfPath().equals(PrometheusConfiguration.get().getAdditionalPath())) {
-            checkPermission(Metrics.VIEW);
             if (collectorRegistry == null) {
                 collectorRegistry = CollectorRegistry.defaultRegistry;
                 collectorRegistry.register(jobCollector);
-                if (Metrics.metricRegistry() != null) {
-                    collectorRegistry.register(new DropwizardExports(Metrics.metricRegistry()));
-                }
+                DefaultExports.initialize();
             }
             return MetricsRequest.prometheusResponse(collectorRegistry);
         }
         throw HttpResponses.notFound();
-    }
-
-    private void checkPermission(Permission permission) {
-        if (PrometheusConfiguration.get().isUseAuthenticatedEndpoint()) {
-            Authentication authentication = Jenkins.getAuthentication();
-            if (!Jenkins.getActiveInstance().getACL().hasPermission(authentication, permission)) {
-                String message = Messages.AccessDeniedException2_MissingPermission(authentication.getName(),
-                                                                                   permission.group.title + "/" + permission.name);
-                throw HttpResponses.errorWithoutStack(403, message);
-            }
-        }
     }
 }
